2d35d346e67ecae53034a79dece9b91ea727ece2
Artur Kamalov <artur.kamalov@akvelon.us>
2025-05-13 21:38:13 +0200

    remove unneded provider

--- adk/terraform/main.tf
+++ adk/terraform/main.tf
@@ -12,17 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-terraform {
-
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project_id
 }
