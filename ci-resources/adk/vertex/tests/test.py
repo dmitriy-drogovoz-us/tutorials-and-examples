bf87ef3913231cafcdb56898ac10fa13d4aa378c
Dima Drogovoz <drogowoz@gmail.com>
2025-07-04 16:56:22 +0100

    check trigger

--- ci-resources/adk/vertex/tests/test.py
+++ ci-resources/adk/vertex/tests/test.py
@@ -2,6 +2,8 @@ import sys
 import requests
 import json
 
+print("TEST TRIGGER!")
+
 def test_model_interaction(base_url):
     url = f'{base_url}/apps/capital_agent/users/1/sessions'
     headers = {
